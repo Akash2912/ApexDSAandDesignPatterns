e0867e35b7066eb1c70e01673e98d6088036decc
Akash Saxena <akashdsaxena@Akashs-MacBook-Air.local>
2021-07-20 16:26:04 +0530

    Added DFS methods for Tree Traversals

--- force-app/main/default/JS_Files/BST.js
+++ force-app/main/default/JS_Files/BST.js
@@ -123,6 +123,27 @@ class BinarySearchTree {
 
         traverse(current);
     }
+    // data[3,8,6,20,15,10]
+
+    /*
+            10
+        6       15
+      3   8        20
+    */
+
+    DFSFirstInOrder(){
+        var data = [];
+        var current = this.root;
+
+        function traverse(node){
+            if(node.left) traverse(node.left);
+            data.push(node.value);
+            if(node.right) traverse(node.right);  
+        }
+
+        traverse(current);
+    }
+    //data[3,6,8,10,15,20]
 }
 
 
